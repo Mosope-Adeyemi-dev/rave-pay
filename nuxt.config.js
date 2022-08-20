7df5050097d3f426c73ed07081c6c77f0fdb4520
Mosope-Adeyemi-dev <78312219+Mosope-Adeyemi-dev@users.noreply.github.com>
2022-08-21 00:04:01 +0100

    change base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,8 +44,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: 'https://bu-paywallet.herokuapp.com/api/v1/',
-    baseURL: 'http://localhost:4000/api/v1/',
+    baseURL: 'https://bu-paywallet.herokuapp.com/api/v1/',
+    // baseURL: 'http://localhost:4000/api/v1/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
